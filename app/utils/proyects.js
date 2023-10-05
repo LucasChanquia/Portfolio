2bfbd4a1423425a69384e4b8582e0170fbf13f35
LucasChanquia <lucaschanquia@gmail.com>
2023-10-05 18:28:26 -0300

    Add proyect in portfolio

--- app/utils/proyects.js
+++ app/utils/proyects.js
@@ -30,6 +30,15 @@
     descriptionEn: "Countries is an API-based application that provides information about 250 countries. In it, you can filter by different items and also add tourist activities.",
     technologies: 'HTML | CSS | JavaScript | React | Node Js | Redux | PosgreSQL | Sequelize',
     link: 'https://cr-pi-countries-main-pi.vercel.app/'
+  },
+  {
+    name: `< Inventario />`,
+    image: '/images/inventario/Captura de pantalla 2023-10-05 181505.png',
+    description: 'Inventario es una aplicación pensada para administrar el stock físico de cualquier negocio o microemprendimiento ya que posee un sistema de usuarios como también de productos con sus respectivas categorías que pueden ser modificadas, eliminadas o creadas tanto como lo necesite el usuario ya que posee un CRUD completo para cada una de las áreas con la que el cliente puede interactuar. Usuario: User || Contraseña: user123'
+    ,
+    descriptionEn: "Inventario is an application designed to manage the physical inventory of any business or microenterprise. It features a user system as well as products with their respective categories that can be modified, deleted, or created as needed by the user. It offers a complete CRUD (Create, Read, Update, Delete) functionality for each of these areas with which the customer can interact. User: User || Password: user123",
+    technologies: 'PHP | HTML | CSS | JavaScript | MySQL',
+    link: 'https://inventarionegocio.000webhostapp.com/app/inventario_php/index.php'
   }
 ];
 
